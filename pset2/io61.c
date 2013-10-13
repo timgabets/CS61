ee21ebc71ab3aba73031bce15d3d979808660834
gabets@g.harvard.edu <tim@gabets.ru>
2013-10-13 20:55:42 +0400

    Test 17 passed

--- pset2/io61.c
+++ pset2/io61.c
@@ -10,9 +10,10 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <errno.h>
+#include <sys/mman.h>
 
 #define STANDALONE      -1
-#define NUMBEROFSLOTS   5
+#define NUMBEROFSLOTS   8192
 #define BUFSIZE         8192
 #define SUCCESS         0
 #define FAIL            -1
@@ -22,15 +23,27 @@
 struct io61_file {
     int     fd;
     int     mode;
+    int     seq;
+    off_t   pos;
 };
 
 typedef struct cacheslot{
     io61_file*  address;
-    char        data[BUFSIZE];
-    //char*       data;
-    size_t     offset;
-    size_t     bufsize;
+    //char        data[BUFSIZE];
+    io61_file*  inf;
+    io61_file*  outf;
+    char*       data;
+    size_t      offset;
+    size_t      bufsize;
+    off_t       pos;        // file position
 }cacheslot;
+
+typedef struct filerelation{
+    io61_file* inf;     // reading from this file
+    io61_file* outf;    // writing to this file
+}filerelation;
+
+filerelation rw;       
 
 struct cacheslot cache[NUMBEROFSLOTS];
 
@@ -40,6 +53,13 @@ int toevict = 0;        // index of the cache slot, that will be evicted next
 int io61_getslot(io61_file*);
 void io61_cacheinit(void);
 int io61_evict(void);
+ssize_t io61_write_mmap(io61_file*, const char*, size_t);
+ssize_t io61_write_seq(io61_file*, const char*, size_t);
+ssize_t io61_read_mmap(io61_file*, char*, size_t);
+ssize_t io61_read_seq(io61_file*, char*, size_t);
+int io61_sortcache(io61_file*);
+void quicksort(int, int);
+
 
 /**
  * [io61_fdopen return a new io61_file that reads from and/or writes to the given file descriptor `fd`.
@@ -49,11 +69,17 @@ int io61_evict(void);
  * @return      [description]
  */
 io61_file* io61_fdopen(int fd, int mode) {
-    
+
     assert(fd >= 0);
     io61_file* f = (io61_file*) malloc(sizeof(io61_file));
     f -> fd = fd;
     f -> mode = mode;
+    f -> seq = TRUE;        // file is sequential by default
+
+    if(mode == O_RDONLY)
+        rw.inf = f;
+    else
+        rw.outf = f;        
 
     return f;
 }
@@ -66,6 +92,7 @@ io61_file* io61_fdopen(int fd, int mode) {
  */
 int io61_close(io61_file* f) {
     io61_flush(f);
+
     int r = close(f->fd);
     free(f);
 
@@ -138,6 +165,20 @@ int io61_writec(io61_file* f, int ch)
     return SUCCESS;
 }
 
+/**
+ * [io61_read description]
+ * @param  f   [description]
+ * @param  buf [description]
+ * @param  sz  [description]
+ * @return     [description]
+ */
+ssize_t io61_read(io61_file* f, char* buf, size_t sz){
+    
+    if(f -> seq == TRUE)   // sequential file
+        return io61_read_seq(f, buf, sz);
+    else        // random access file
+        return io61_read_mmap(f, buf, sz);
+}
 
 /**
  * [io61_read Read up to `sz` characters from `f` into `buf`.]
@@ -148,7 +189,7 @@ int io61_writec(io61_file* f, int ch)
  *              Returns a short count if the file ended before `sz` characters could be read. 
  *              Returns -1 an error occurred before any characters were read.]
  */
-ssize_t io61_read(io61_file* f, char* buf, size_t sz) {
+ssize_t io61_read_seq(io61_file* f, char* buf, size_t sz) {
 
     ssize_t nread;
 
@@ -181,16 +222,52 @@ ssize_t io61_read(io61_file* f, char* buf, size_t sz) {
         return sz;
 }
 
+/**
+ * [io61_read_mmap description]
+ * @param  f   [description]
+ * @param  buf [calling programm expects that this buffer will be used for data interchange, 
+ *              but we use it i a different way - buf is storing the pointer to read file structure. 
+ *              ]
+ * @param  sz  [description]
+ * @return     [description]
+ */
+ssize_t io61_read_mmap(io61_file* f, char* buf, size_t sz) {
+    (void) buf;
+    (void) f;
+
+    int i = io61_getslot(f);
+    cache[i].pos = f -> pos;
+    cache[i].data = mmap(NULL, sz, PROT_READ, MAP_PRIVATE, f -> fd, cache[i].pos);
+    cache[i].bufsize = sz;
+
+    return sz;
+}
+
 
 /**
- * [io61_write writes `sz` characters from `buf` to `f`]
+ * [io61_write write function wrapper]
+ * @param  f   [description]
+ * @param  buf [description]
+ * @param  sz  [description]
+ * @return     [description]
+ */ 
+ssize_t io61_write(io61_file* f, const char* buf, size_t sz) {
+
+    if(f -> seq == TRUE)   // sequential file
+        return io61_write_seq(f, buf, sz);
+    else        // random access file
+        return io61_write_mmap(f, buf, sz);
+}
+
+/**
+ * [io61_write_seq writes `sz` characters from `buf` to `f`. Used for sequential files.]
  * @param  f   [description]
  * @param  buf [description]
  * @param  sz  [description]
  * @return     [Returns the number of characters written on success; normally this is `sz`. 
  *              Returns -1 if an error occurred before any characters were written.]
  */
-ssize_t io61_write(io61_file* f, const char* buf, size_t sz) {
+ssize_t io61_write_seq(io61_file* f, const char* buf, size_t sz) {
 
     for(int i = 0; i < NUMBEROFSLOTS; i++)
     {
@@ -218,20 +295,22 @@ ssize_t io61_write(io61_file* f, const char* buf, size_t sz) {
     cache[i].offset = sz;
 
     return sz;
-/*
-    size_t nwritten = 0;
-    while (nwritten != sz) {
-        if (io61_writec(f, buf[nwritten]) == -1)
-            break;
-        ++nwritten;
-    }
-    if (nwritten == 0 && sz != 0)
-        return -1;
-    else
-        return nwritten;
-*/
 }
 
+/**
+ * [io61_write_mmap writes `sz` characters from `buf` to `f`, using mmap IO.
+ *                          Used for random access files.]
+ * @param  f   [file]
+ * @param  buf [buffer to write]
+ * @param  sz  [size of buffer]
+ * @return     [Returns the number of characters written on success; normally this is `sz`. 
+ *              Returns -1 if an error occurred before any characters were written.]
+ */ 
+ssize_t io61_write_mmap(io61_file* f, const char* buf, size_t sz) {
+    (void) buf;
+    (void) f;
+    return sz;
+}
 
 /**
  * [io61_seek change the file pointer for file `f` to `pos` bytes into the file.]
@@ -240,9 +319,15 @@ ssize_t io61_write(io61_file* f, const char* buf, size_t sz) {
  * @return     [returns 0 on success and -1 on failure.]
  */
 int io61_seek(io61_file* f, size_t pos) {
+
     off_t r = lseek(f->fd, (off_t) pos, SEEK_SET);
+    f -> seq = FALSE;
+
     if (r == (off_t) pos)
+    {
+        f -> pos = r;
         return 0;
+    }
     else
         return -1;
 }
@@ -267,7 +352,12 @@ int io61_getslot(io61_file* f)
         if(cache[i].address == NULL)
         {    
             cache[i].address = f;
-            //cache[i].data = malloc(BUFSIZE);
+
+            if(f -> seq == FALSE && f -> mode == O_WRONLY)
+                cache[i].data = malloc( sizeof(void*) );
+            else
+                cache[i].data = malloc(BUFSIZE);
+
             cache[i].offset = 0;
             cache[i].bufsize = BUFSIZE;
             
@@ -280,7 +370,10 @@ int io61_getslot(io61_file* f)
 
     // the last rites:   
     cache[i].address = f;
-    //cache[i].data = malloc(BUFSIZE);
+    if(f -> seq == FALSE && f -> mode == O_WRONLY)
+        cache[i].data = malloc( sizeof(void*) );
+    else
+        cache[i].data = malloc(BUFSIZE);
     cache[i].offset = 0;
     cache[i].bufsize = BUFSIZE;
    
@@ -298,7 +391,7 @@ int io61_evict(void)
     if( (cache[i].address) -> fd == O_WRONLY)
         io61_flush(cache[toevict].address);
 
-    //free(cache[i].data);
+    free(cache[i].data);
 
     toevict++;
     toevict %= NUMBEROFSLOTS;
@@ -313,7 +406,10 @@ int io61_evict(void)
 void io61_cacheinit(void)
 {
     for(int i = 0; i < NUMBEROFSLOTS; i++)
+    {
         cache[i].address = NULL;
+        cache[i].pos = INT_MAX;
+    }
 }
 
 
@@ -323,24 +419,99 @@ void io61_cacheinit(void)
  * @return   [description]
  */
 int io61_flush(io61_file* f) {
-    (void) f;
+    //(void) f;
+    if(f -> mode == O_RDONLY)
+        return 0;
 
-    // searching for a slot:
-    for(int i = 0; i < NUMBEROFSLOTS; i++)
-        if(cache[i].address == f)
-        {
-            if( write(f -> fd, cache[i].data, cache[i].offset) != -1)
+
+    if( f -> seq == TRUE)
+    {
+        // searching for a slot:
+        for(int i = 0; i < NUMBEROFSLOTS; i++)
+            if(cache[i].address == f)
             {
-                cache[i].address = NULL;           
-                return SUCCESS;
-            }else
-                return FAIL;
+                if( write(f -> fd, cache[i].data, cache[i].offset) != -1)
+                {
+                    cache[i].address = NULL;           
+                    free(cache[i].address);
+                    return SUCCESS;
+                }else
+                    return FAIL;
+            }
+    }else    
+    {
+        /**
+         * file is non-sequential, and the cache slots may contain data in random order.
+         * We need to sort cache slots by 'pos' field in increasing order. 
+         */
+        if( io61_sortcache(f) == SUCCESS )
+
+        lseek(f -> fd, 0, SEEK_SET);
+        for(int i = 0; i < NUMBEROFSLOTS; i++)
+        {
+            if(cache[i].address == rw.inf)
+            {
+                write(f -> fd, cache[i].data, cache[i].bufsize);
+                munmap(cache[i].data, cache[i].bufsize);
+                //free(cache[i].data);
+            }
         }
+
+        return SUCCESS;
+    }
 
     return FAIL;
 }
 
+/**
+ * [io61_cachesort sorts the cache array by 'pos' field in increasing order. ]
+ * @return [0 on success, -1 on fail]
+ */
+int io61_sortcache(io61_file* f)
+{
+    (void) f;
+    // sorting cache array:
+    quicksort(0, NUMBEROFSLOTS);
 
+    return SUCCESS;
+}
+
+
+/**
+ * [quicksort classic quick sort algorithm]
+ * @param first [index of the first element in a scope]
+ * @param last  [index of the last element inthe scope]
+ */
+void quicksort(int first, int last)
+{
+    cacheslot temp;
+    int i = first, 
+    j = last, 
+    x = cache[(first + last) / 2].pos;
+ 
+    do {
+        while (cache[i].pos < x) i++;
+        while (cache[j].pos > x) j--;
+ 
+        if(i <= j) {
+            if (i < j)
+            {
+                temp = cache[i];
+                cache[i] = cache[j];
+                cache[j] = temp;
+            } 
+            i++;
+            j--;
+        }
+    } while (i <= j);
+ 
+    if (i < last)
+        quicksort(i, last);
+    if (first < j)
+        quicksort(first,j);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 // You should not need to change either of these functions.
 
 // io61_open_check(filename, mode)
